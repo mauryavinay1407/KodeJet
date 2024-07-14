c1fbdbe2f7711cbd2135e88611348853e2ce7697
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-07-14 18:43:09 +0530

    C++ compiler added

---
+++ server/codes/8bcb457b-8056-4786-a3da-16c04727efd4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+
+using namespace std;
+
+int findCoin(vector<int> arr,int target)
+{
+    if(target==0)
+    return 0;
+    if(target<0)
+    return INT_MAX;
+    int minimum=INT_MAX;
+
+    for(int i=0;i<arr.size();i++)
+    {
+       int ans=findCoin(arr,target-arr[i]);
+       if(ans!=INT_MAX)
+       minimum=min(minimum,ans+1);
+    }
+
+    return minimum;
+}
+
+int main()
+{
+    vector <int> arr{1,2,3};
+    int target=5;
+
+    cout<<findCoin(arr,target);
+
+    return 0;
+}
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const app=express();
+const {generateFile}=require('./generateFile');
+const { executeCpp } = require("./executeCpp");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+   return res.json({
+    hello:"World"
+   });
+})
+app.post('/run',async(req,res)=>{
+    const {language="cpp",code}=req.body;
+    if(!code)
+        return res.status(400).json({success:false,error:"Empty code body ain't allowed"});
+    
+    try {
+    const filepath=await generateFile(language,code);
+    const output=await executeCpp(filepath);
+    console.log(output);
+    return res.json({filepath,output});
+    } catch (error) {
+     return res.status(400).json({error})    
+    }
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running at http://localhost:3000");
+})
