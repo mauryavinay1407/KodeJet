17057ba029d818ea6ad8edf58eb21c1b84420979
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-08-04 00:50:36 +0530

    schema updated

--- server/models/job.model.js
+++ server/models/job.model.js
@@ -4,7 +4,7 @@ const JobSchema=new mongoose.Schema({
     language:{
          type: String,
          required: true,
-         enum:["cpp","py"]
+         enum:["cpp","py","js","c"]
     },
     filepath:{
         type: String,
